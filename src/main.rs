f5bafa8b20b620dbf07cbba24727292164a38392
Owen Boreham <bobrossrtx@gmail.com>
2021-07-02 19:45:24 +0100

    main.rs main entry point

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+#![feature(decl_macro)]
+#[macro_use] extern crate rocket;
+
+use rocket::Request;
+use rocket::request::Form;
+use rocket::response::content::Json;
+use rocket_contrib::templates::Template;
+use serde::Serialize;
+
+#[derive(FromForm, Debug)]
+struct Book {
+	title: String,
+	author: String,
+	isbn: String
+}
+
+/* ============================ */
+/* Routes												*/
+/* ============================ */
+
+// === Gets ===
+
+#[get("/")]
+fn index() -> Template {
+	#[derive(Serialize)]
+	struct Context {
+		first_name: String,
+		last_name: String
+	}
+	let context = Context {
+		first_name: String::from("Owen"),
+		last_name: String::from("Boreham")
+	};
+	Template::render("home", context)
+}
+
+#[get("/hello")]
+fn hello() -> Json<&'static str> {
+	let response = "
+{
+	'status': 'success',
+	'message': 'Hello API!'
+}
+	";
+	Json(response)
+}
+
+// === Posts ===
+
+#[post("/book", data = "<book_form>")]
+fn new_book(book_form: Form<Book>) -> String {
+  let book: Book = book_form.into_inner();
+  let mut dummy_db: Vec<Book> = Vec::new();
+  dummy_db.push(book);
+  format!("Book added successfully: {:?}", dummy_db)
+}
+
+// === Errors ===
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+	format!("Oh no! We couldn't find the requested path '{}'", req.uri())
+}
+
+/* ============================ */
+/* main													*/
+/* ============================ */
+
+fn main() {
+	rocket::ignite()
+		.register(catchers!(not_found))
+		.mount("/", routes![index])
+		.mount("/api", routes![hello, new_book])
+		.attach(Template::fairing())
+		.launch();
+}
